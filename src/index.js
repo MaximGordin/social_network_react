4df7cad97779e2ffedb4a6b5ffa9c9b088fe65d6
maxim <maxim@etistudio.com>
2019-05-13 23:59:03 +0300

    123

--- src/index.js
+++ src/index.js
@@ -3,24 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import state from'./redux/state.js';
-import {addPost,subscribe, updateNewPostChange} from'./redux/state.js';
+import store from'./redux/state.js';
+// import {addPost,subscribe, updateNewPostChange} from'./redux/state.js';
 
 
 let renderEntireTree = (state) =>{
+       
     ReactDOM.render(<App 
-        addPost={addPost}
+        addPost={store.addPost.bind(store)}
+        updateNewPostChange={store.updateNewPostChange.bind(store)}
         newPostText={state.profilePage.newPostText}
-        updateNewPostChange={updateNewPostChange}
         messagesData={state.dialogsPage.messagesData}
         dialogsData={state.dialogsPage.dialogsData}
         postData={state.profilePage.postData}
         friendsData={state.aside.friendsData} />, document.getElementById('root'));
 }
 
-renderEntireTree(state);
+renderEntireTree(store.getState());
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,60 +1,64 @@
-let renderEntireTree = () => {
-    console.log(1);
-}
 
-let state = {
-    dialogsPage: {
-        dialogsData: [
-            { id: 1, name: 'Катя' },
-            { id: 2, name: 'Петя' },
-            { id: 3, name: 'Валя' },
-            { id: 4, name: 'Вася' }
-        ],
-        messagesData: [
-            { id: 1, message: 'Hello' },
-            { id: 2, message: 'How are you' },
-            { id: 3, message: 'Well' }
-        ]
+let store = {
+    _state: {
+        dialogsPage: {
+            dialogsData: [
+                { id: 1, name: 'Катя' },
+                { id: 2, name: 'Петя' },
+                { id: 3, name: 'Валя' },
+                { id: 4, name: 'Вася' }
+            ],
+            messagesData: [
+                { id: 1, message: 'Hello' },
+                { id: 2, message: 'How are you' },
+                { id: 3, message: 'Well' }
+            ]
+        },
+    
+        profilePage: {
+            postData: [
+                { id: 1, message: 'Post 1' },
+                { id: 2, message: 'Post 2' },
+                { id: 3, message: 'Post 3' },
+                { id: 4, message: 'Post 4' }
+            ],
+            newPostText: ""
+        },
+    
+        aside: {
+            friendsData: [
+                {id: 1, name: 'Катя'},
+                {id: 2, name: 'Петя'},
+                {id: 3, name: 'Валя'}
+            ]
+        }
     },
-
-    profilePage: {
-        postData: [
-            { id: 1, message: 'Post 1' },
-            { id: 2, message: 'Post 2' },
-            { id: 3, message: 'Post 3' },
-            { id: 4, message: 'Post 4' }
-        ],
-        newPostText: ""
+    getState() {
+        return this._state;
     },
-
-    aside: {
-        friendsData: [
-            {id: 1, name: 'Катя'},
-            {id: 2, name: 'Петя'},
-            {id: 3, name: 'Валя'}
-        ]
+    renderEntireTree() {
+        console.log(1);
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText
+        }
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this.renderEntireTree(this._state)
+    },
+    updateNewPostChange(newText) {
+        this._state.profilePage.newPostText = newText;
+        this.renderEntireTree(this._state)
+    },
+    subscribe(observer) {
+        this.renderEntireTree = observer;
     }
 }
 
 
-let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText
-    }
-    state.profilePage.postData.push(newPost);
-    state.profilePage.newPostText = "";
-    renderEntireTree(state)
-}
+// export  {addPost,subscribe,updateNewPostChange};
+export default store;
 
-let updateNewPostChange = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state)
-}
-
-const subscribe = (observer) => {
-    renderEntireTree = observer;
-}
-
-export  {addPost,subscribe,updateNewPostChange};
-export default state;
+window.store = store;
